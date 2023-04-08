5c12a5d8c6e56a4f6d143d14cec94a9db8f905d3
20230683 <129809426+20230683@users.noreply.github.com>
2023-04-08 12:33:23 +1200

    Datetime import test

---
+++ import datetime.py
@@ -0,0 +1,6 @@
+#this is a test script in python that uses the datetime function to import it, then print the current date and time
+import datetime
+today = datetime.date.today()
+print(today)
+now = datetime.datetime.now()
+print(now)
