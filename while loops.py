4d9589cb6fc69f8a9ed2cd3b70fee9f5828b115e
20230683 <129809426+20230683@users.noreply.github.com>
2023-04-08 12:31:31 +1200

    Test while loop script

---
+++ while loops.py
@@ -0,0 +1,6 @@
+#this is a test script in python that uses a while loop, and counts up to 10
+#this was good to learn as i needed to use it in my software project
+count = 0
+while count < 10:
+    print("Count is", count)
+    count += 1
