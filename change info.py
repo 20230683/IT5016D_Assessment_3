a2f907ca789927cbcdc5ee03d42112371fd60972
20230683 <129809426+20230683@users.noreply.github.com>
2023-04-08 12:51:54 +1200

    This changes the info in a container

---
+++ change info.py
@@ -0,0 +1,6 @@
+#this script defines the objects with an age, so Emma = 24
+#i think change Emmas age to 42, then print her age to show the change
+
+friendsage={"Tom":"25", "Emma":"24", "Chloe":"25"}
+friendsage["Emma"]=42
+print(friendsage["Emma"])
